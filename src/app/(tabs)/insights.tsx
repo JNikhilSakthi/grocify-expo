0c224b1e062d0de7d2ae86152c320ae9b8ee1355
burakorkmez <burakorkmezz@gmail.com>
2026-03-13 15:59:50 +0800

    feat(tabs): implement tab navigation with user authentication and screens

---
+++ src/app/(tabs)/insights.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+const InsightsScreen = () => {
+  return (
+    <View>
+      <Text>InsightsScreen</Text>
+    </View>
+  );
+};
+export default InsightsScreen;
